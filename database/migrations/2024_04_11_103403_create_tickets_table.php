ad4700872dbd2548ffe90d8921755690208dbd8d
PandoraCipher <www.antso@gmail.com>
2024-04-12 10:33:10 +0300

    interface graphique

--- database/migrations/2024_04_11_103403_create_tickets_table.php
+++ database/migrations/2024_04_11_103403_create_tickets_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('assigned');
+            $table->string('status');
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/main', function(){
+    return view('layouts/main');
+});
